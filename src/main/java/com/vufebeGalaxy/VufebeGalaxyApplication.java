725abbd88929f098cb80f75ae0cfe9fbc14da2f4
Josimar Sanchez <josanchezag@gmail.com>
2022-06-15 15:57:27 -0500

    Create shapes application
    Create shapes application

---
+++ src/main/java/com/vufebeGalaxy/VufebeGalaxyApplication.java
@@ -0,0 +1,13 @@
+package com.vufebeGalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VufebeGalaxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VufebeGalaxyApplication.class, args);
+	}
+
+}
